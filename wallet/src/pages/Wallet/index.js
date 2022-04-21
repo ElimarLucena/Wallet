13065ea6fc372b020cf8405100d4a088bc172c5b
Elimar Lucena <elimarlucenar@gmail.com>
2022-04-21 09:43:14 -0300

    style: correcting the indentation

--- wallet/src/pages/Wallet/index.js
+++ wallet/src/pages/Wallet/index.js
@@ -56,89 +56,89 @@ function Wallet() {
 
   return (
     <div>
-        <Header />
-        <form>
-          <label htmlFor="value">
-            Value:
-            <input
-              type="number"
-              name="value"
-              value={ value }
-              onChange={ ({ target: { value } }) => setValue(value) }
-            />
-          </label>
-          <label htmlFor="currency">
-            Currency:
-            <select
-              name="currency"
-              onChange={ ({ target: { value } }) => setCurrency(value) }
+      <Header />
+      <form>
+        <label htmlFor="value">
+          Value:
+          <input
+            type="number"
+            name="value"
+            value={ value }
+            onChange={ ({ target: { value } }) => setValue(value) }
+          />
+        </label>
+        <label htmlFor="currency">
+          Currency:
+          <select
+            name="currency"
+            onChange={ ({ target: { value } }) => setCurrency(value) }
+          >
+            {
+              currencyName.filter((item) => item !== 'USDT').map((item) => (
+                <option
+                  key={ item }
+                  value={ item }
+                  data-testid={ item }
+                >
+                  { item }
+                </option>
+              ))
+            }
+          </select>
+        </label>
+        <label htmlFor="description">
+        Description:
+          <input
+            name="description"
+            type="text"
+            value={ description }
+            onChange={ ({ target: { value } }) => setDescription(value) }
+          />
+        </label>
+        <label htmlFor="method">
+          Method:
+          <select
+            name="method"
+            onChange={ ({ target: { value } }) => setMethod(value) }
+          >
+            <option>Money</option>
+            <option>Credit card</option>
+            <option>Debit card</option>
+          </select>
+        </label>
+        <label htmlFor="category">
+          Category:
+          <select
+            name="category"
+            onChange={ ({ target: { value } }) => setCategory(value) }
+          >
+            <option>Food</option>
+            <option>Leisure</option>
+            <option>Work</option>
+            <option>Transport</option>
+            <option>Health</option>
+          </select>
+        </label>
+        {
+          Object.keys(editList).length > 0 ? (
+            <button>
+              Edit Expenses
+            </button>
+          ) : (
+            <button
+              type="button"
+              onClick={ () => {
+                setExpenses([...expenses, states]);
+                setId(id + 1);
+              } }
             >
-              {
-                currencyName.filter((item) => item !== 'USDT').map((item) => (
-                  <option
-                    key={ item }
-                    value={ item }
-                    data-testid={ item }
-                  >
-                    { item }
-                  </option>
-                ))
-              }
-            </select>
-          </label>
-          <label htmlFor="description">
-          Description:
-            <input
-              name="description"
-              type="text"
-              value={ description }
-              onChange={ ({ target: { value } }) => setDescription(value) }
-            />
-          </label>
-          <label htmlFor="method">
-            Method:
-            <select
-              name="method"
-              onChange={ ({ target: { value } }) => setMethod(value) }
-            >
-              <option>Money</option>
-              <option>Credit card</option>
-              <option>Debit card</option>
-            </select>
-          </label>
-          <label htmlFor="category">
-            Category:
-            <select
-              name="category"
-              onChange={ ({ target: { value } }) => setCategory(value) }
-            >
-              <option>Food</option>
-              <option>Leisure</option>
-              <option>Work</option>
-              <option>Transport</option>
-              <option>Health</option>
-            </select>
-          </label>
-          {
-            Object.keys(editList).length > 0 ? (
-              <button>
-                Edit Expenses
-              </button>
-            ) : (
-              <button
-                type="button"
-                onClick={ () => {
-                  setExpenses([...expenses, states]);
-                  setId(id + 1);
-                } }
-              >
-                Add Expenses
-              </button>
-            )
-          }
-        </form>
-        <Table />
-      </div>
+              Add Expenses
+            </button>
+          )
+        }
+      </form>
+      <Table />
+    </div>
   )
 }
 
